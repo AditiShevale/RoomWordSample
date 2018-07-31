7c86643895c949a2e016ce92c27bd6d501857803
aditishevale <aditi.shevale999@gmail.com>
2018-07-31 17:59:38 +0530

    WordRepository Updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/WordRespository.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/WordRespository.java
@@ -1,6 +1,8 @@
 package com.example.aditishevale.roomwordsample;
 
+import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
 
 import java.util.List;
 
@@ -8,4 +10,36 @@ public class WordRespository {
 
     private WordDao mWordDao;
     private LiveData<List<Word>> mAllWords;
+
+
+    WordRespository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+
+        @Override
+        protected Void doInBackground(Word... params) {
+
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
 }
