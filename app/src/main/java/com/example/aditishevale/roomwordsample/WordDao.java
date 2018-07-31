41f7ed126fcde5ac95b09eae45c4ed5376e343b1
aditishevale <aditi.shevale999@gmail.com>
2018-07-31 17:44:05 +0530

    Word Class Updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/WordDao.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/WordDao.java
@@ -1,6 +1,5 @@
 package com.example.aditishevale.roomwordsample;
 
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
@@ -15,9 +14,9 @@ public interface WordDao {
     @Insert
     void insert(Word word);
 
-      @Query("DELETE FROM word_table")
+    @Query("DELETE FROM word_table")
     void deleteAll();
 
     @Query("SELECT * from word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+      LiveData<List<Word>> getAllWords();
 }
