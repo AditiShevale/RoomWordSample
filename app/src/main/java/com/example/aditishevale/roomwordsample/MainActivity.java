f1228f9424b8e22ce77dc7335a0f46e0f329904b
aditishevale <aditi.shevale999@gmail.com>
2018-07-31 18:21:26 +0530

    WordViewMOdel Updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/MainActivity.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
