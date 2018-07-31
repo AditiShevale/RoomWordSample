1f5e5ed85193cc18caaad496749bf6b2166b3b40
aditishevale <aditi.shevale999@gmail.com>
2018-07-31 17:11:39 +0530

    Word Class Created

---
+++ app/src/main/java/com/example/aditishevale/roomwordsample/Word.java
@@ -0,0 +1,15 @@
+package com.example.aditishevale.roomwordsample;
+
+import android.support.annotation.NonNull;
+
+public class Word {
+
+    private String mWord;
+
+    public Word(@NonNull String word)
+    {
+        this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+
+}
