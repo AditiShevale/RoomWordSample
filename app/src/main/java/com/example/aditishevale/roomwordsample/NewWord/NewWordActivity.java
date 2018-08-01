b4ca73d9c0f2c2ebbe4f43e4de0c4fae46209154
aditishevale <aditi.shevale999@gmail.com>
2018-08-01 11:57:36 +0530

    Adding New Words

---
+++ app/src/main/java/com/example/aditishevale/roomwordsample/NewWord/NewWordActivity.java
@@ -0,0 +1,15 @@
+package com.example.aditishevale.roomwordsample.NewWord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aditishevale.roomwordsample.R;
+
+public class NewWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_word);
+    }
+}
