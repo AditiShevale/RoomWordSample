c702221737bebb12298a066a1d5caff96b898ba0
Aditi Shevale <aditi.shevale999@gmail.com>
2018-08-31 22:46:26 +0530

    updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/NewWord/NewWordActivity.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/NewWord/NewWordActivity.java
@@ -23,6 +23,9 @@ public class NewWordActivity extends AppCompatActivity {
 
     @Override
 
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
