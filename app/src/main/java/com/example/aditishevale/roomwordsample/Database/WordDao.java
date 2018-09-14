ce152ad307b45296136b8d980b5dc7f13a95e298
Aditi Shevale <aditi.shevale999@gmail.com>
2018-09-14 17:21:45 +0530

    updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/Database/WordDao.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/Database/WordDao.java
@@ -8,7 +8,6 @@ import android.arch.persistence.room.Query;
 import java.util.List;
 
 @Dao
-
 public interface WordDao {
 
     @Insert
--- app/src/main/java/com/example/aditishevale/roomwordsample/ViewModel/WordViewModel.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/ViewModel/WordViewModel.java
@@ -24,7 +24,8 @@ public class WordViewModel extends AndroidViewModel {
 
         mAllWords = mRepository.getAllWords();
     }
-   public LiveData<List<Word>> getAllWords() {
+
+    public LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
