1521b8fc927ccf39608afde27824177eaec08cfe
Aditi Shevale <aditi.shevale999@gmail.com>
2018-09-14 17:20:41 +0530

    updated

--- app/src/main/java/com/example/aditishevale/roomwordsample/Database/WordDao.java
+++ app/src/main/java/com/example/aditishevale/roomwordsample/Database/WordDao.java
@@ -8,13 +8,17 @@ import android.arch.persistence.room.Query;
 import java.util.List;
 
 @Dao
+
 public interface WordDao {
+
     @Insert
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
+
     void deleteAll();
 
     @Query("SELECT * from word_table ORDER BY word ASC")
+
       LiveData<List<Word>> getAllWords();
 }
